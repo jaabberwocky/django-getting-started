5bc587c88966927aff974b8b49fc47108b690fd1
tobias <tobias@dsaid.gov.sg>
2020-08-24 02:13:15 +0800

    Finish part 7

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # customise admin panel for Questions
+    fieldsets = [
+        ('Basic', {'fields': ['question_text']}),
+        ('Date info', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
